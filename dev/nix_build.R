d56d8f578f16b84ab85fd55b606b5da8f910077b
Eric Nantz <theRcast@gmail.com>
2025-05-23 13:42:30 -0400

    chore: add pkglite to nix dev environment

--- dev/nix_build.R
+++ dev/nix_build.R
@@ -3,10 +3,10 @@ library(rix)
 
 rix(
   r_ver = "4.4.0",
-  r_pkgs = c("dplyr", "kableExtra", "magrittr", "desc", "purrr", "renv", "quarto"),
+  r_pkgs = c("dplyr", "kableExtra", "magrittr", "desc", "purrr", "renv", "quarto", "pkglite"),
   system_pkgs = c("dejavu_fonts", "quarto"),
   tex_pkgs = c("amsmath", "framed", "multirow", "wrapfig", "tabu", "threeparttable", "threeparttablex", "environ", "makecell", "tcolorbox", "tikzfill", "pdfcol", "fontawesome5"),
-  ide = "other",
+  ide = "none",
   project_path = ".",
   overwrite = TRUE,
   print = TRUE
