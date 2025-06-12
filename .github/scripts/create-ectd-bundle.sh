0c0f3b0ce8f0ace496f722ccc33ba8a8817b5ef4
Eric Nantz <theRcast@gmail.com>
2025-06-11 23:48:26 -0400

    fix: copy data files properly in ectd bash script

--- .github/scripts/create-ectd-bundle.sh
+++ .github/scripts/create-ectd-bundle.sh
@@ -15,6 +15,7 @@ DOCKER_SOURCE_FILE=Dockerfile.txt
 ENTRYPOINT_FILE=entrypoint.R
 ECTD_BUNDLE_DIR=submissions-pilot4-container-to-fda
 ECTD_LETTER_DIR=${ECTD_BUNDLE_DIR}/m1/us
+ECTD_ROOT_DIR=${ECTD_BUNDLE_DIR}/m5/datasets/rconsortiumpilot4container/analysis/adam
 ECTD_DATASETS_DIR=${ECTD_BUNDLE_DIR}/m5/datasets/rconsortiumpilot4container/analysis/adam/datasets
 ECTD_PROGRAMS_DIR=${ECTD_BUNDLE_DIR}/m5/datasets/rconsortiumpilot4container/analysis/adam/programs
 ADRG_DESTINATION_DIR=${ECTD_DATASETS_DIR}
@@ -34,7 +35,6 @@ DATASETS_SOURCE_DIR=datasets
 # Create directory structure for ectd bundle
 mkdir -p "${ECTD_LETTER_DIR}"
 mkdir -p "${ECTD_PROGRAMS_DIR}"
-#mkdir -p "${ECTD_DATASETS_DIR}"
 
 # Copy ADRG (PDF version)
 if [ -f "${ADRG_SOURCE_DIR}/${ADRG_SOURCE_FILE}" ]; then
@@ -80,49 +80,8 @@ if [ -f "${PKGLITE_SOURCE_DIR}/${PKGLITE_SOURCE_FILE}" ]; then
   cp "${DOCKER_SOURCE_FILE}" "${ECTD_PROGRAMS_DIR}/${DOCKER_SOURCE_FILE}"
 fi
 
-# Copy pilot 2 application files
-# if [ -d "$APP_SOURCE_DIR" ]; then
-#   echo "Copying application files"
-#   if [ ! -f "${ECTD_PROGRAMS_DIR}/${APP_SOURCE_DIR}" ]; then
-#     echo "Create application directory ${ECTD_PROGRAMS_DIR}/${APP_SOURCE_DIR}"
-#     mkdir -p "${ECTD_PROGRAMS_DIR}/${APP_SOURCE_DIR}"
-#     mkdir -p "${ECTD_PROGRAMS_DIR}/${APP_SOURCE_DIR}/inst"
-#   fi
-#   cp -r "${APP_SOURCE_DIR}/inst/app" "${ECTD_PROGRAMS_DIR}/${APP_SOURCE_DIR}/inst/app"
-#   cp "${APP_SOURCE_DIR}/inst/golem-config.yml" "${ECTD_PROGRAMS_DIR}/${APP_SOURCE_DIR}/inst/golem-config.yml"
-#   cp "${APP_SOURCE_DIR}/inst/startup.R" "${ECTD_PROGRAMS_DIR}/${APP_SOURCE_DIR}/inst/startup.R"
-#   cp -r "${APP_SOURCE_DIR}/man" "${ECTD_PROGRAMS_DIR}/${APP_SOURCE_DIR}/man"
-#   cp -r "${APP_SOURCE_DIR}/R" "${ECTD_PROGRAMS_DIR}/${APP_SOURCE_DIR}/R"
-#   cp -r "${APP_SOURCE_DIR}/renv" "${ECTD_PROGRAMS_DIR}/${APP_SOURCE_DIR}/renv"
-#   cp "${APP_SOURCE_DIR}/.Rprofile" "${ECTD_PROGRAMS_DIR}/${APP_SOURCE_DIR}/.Rprofile"
-#   cp "${APP_SOURCE_DIR}/.Rbuildignore" "${ECTD_PROGRAMS_DIR}/${APP_SOURCE_DIR}/.Rbuildignore"
-#   cp "${APP_SOURCE_DIR}/app.R" "${ECTD_PROGRAMS_DIR}/${APP_SOURCE_DIR}/app.R"
-#   cp "${APP_SOURCE_DIR}/DESCRIPTION" "${ECTD_PROGRAMS_DIR}/${APP_SOURCE_DIR}/DESCRIPTION"
-#   cp "${APP_SOURCE_DIR}/LICENSE.md" "${ECTD_PROGRAMS_DIR}/${APP_SOURCE_DIR}/LICENSE.md"
-#   cp "${APP_SOURCE_DIR}/NAMESPACE" "${ECTD_PROGRAMS_DIR}/${APP_SOURCE_DIR}/NAMESPACE"
-#   cp "${APP_SOURCE_DIR}/NEWS.md" "${ECTD_PROGRAMS_DIR}/${APP_SOURCE_DIR}/NEWS.md"
-#   cp "${APP_SOURCE_DIR}/README.md" "${ECTD_PROGRAMS_DIR}/${APP_SOURCE_DIR}/README.md"
-#   cp "${APP_SOURCE_DIR}/renv.lock" "${ECTD_PROGRAMS_DIR}/${APP_SOURCE_DIR}/renv.lock"
-#   cp "${APP_SOURCE_DIR}/submissions-pilot2.Rproj" "${ECTD_PROGRAMS_DIR}/${APP_SOURCE_DIR}/submissions-pilot2.Rproj"
-
-#   echo "Copying Docker container files"
-#   cp "${DOCKER_SOURCE_FILE}" "${ECTD_PROGRAMS_DIR}/${DOCKER_SOURCE_FILE}"
-#   cp "${ENTRYPOINT_FILE}" "${ECTD_PROGRAMS_DIR}/${ENTRYPOINT_FILE}"
-# fi
-
-# Copy pilot 2 data files
+# Copy datasets
 if [ -d "$DATASETS_SOURCE_DIR" ]; then
-  echo "Copying dataset files"
-  cp -r "${DATASETS_SOURCE_DIR}" "${ECTD_DATASETS_DIR}"
+  echo "Copying ${DATASETS_SOURCE_DIR}"
+  cp -r "${DATASETS_SOURCE_DIR}" "${ECTD_ROOT_DIR}"
 fi
-
-# if [ -f "${ECTD_BUNDLE_DIR}/${ECTD_BUNDLE_FILE}" ]; then
-#   echo "Copying ${ECTD_BUNDLE_DIR}/${ECTD_BUNDLE_FILE}"
-#   if [ ! -f "$DESTINATION_DIR" ]; then
-#     echo "Create new directory ${DESTINATION_DIR}"
-#     mkdir -p "${DESTINATION_DIR}"
-#   fi
-#   cp "${ECTD_BUNDLE_DIR}/${ECTD_BUNDLE_FILE}" "${DESTINATION_DIR}/${ECTD_BUNDLE_FILE}"
-# fi
-
-# echo "ECTD Bundle file copied to ${DESTINATION_DIR}/${ECTD_BUNDLE_FILE}"
